4f5b4b2132a8eeea1f5f44ed66f8288204fb6ffe
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-06-12 16:07:58 -0700

    add move class kinda

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/super_computer_player.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/super_computer_player.rb
@@ -2,6 +2,12 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    new_board = TicTacToeNode.new(game, mark)
+    new_board.children.each do |i|
+      if i.winning_node?(i.next_mover_mark)
+        return i.prev_move_pos
+      end
+    end
   end
 end
 
--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
@@ -35,26 +35,39 @@ class TicTacToeNode
 	# if board winner is evaluator, is winning node
 	if board.over?
 		if board.winner == evaluator || board.winner.nil?
-			return false
-		else
 			return true
+		else
+			return false
 		end	
 	end
 	if self.next_mover_mark == evaluator
-		self.children.all? {|child| child.losing_node?(evaluator)}
+		self.children.any? {|child| child.winning_node?(evaluator)}
 	else
-		self.children.any? {|child| child.losing_node?(evaluator)}
+		self.children.all? {|child| child.winning_node?(evaluator)}
 	end
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    res = []
     next_mark = (self.next_mover_mark == :o ? :x : :o)
-    @board.open_positions.map do |i|
+    (0..2).each do |i|
+      (0..2).each do |j|
+        res << [i, j] unless @board.rows[i][j]
+      end
+    end
+    res.map do |i|
       board = @board.dup
       board[i] = self.next_mover_mark
       TicTacToeNode.new(board, next_mark, i)
     end
+  #     board = @board.dup
+  #     board[i] = self.next_mover_mark
+  #     if @board.rows[i][j].nil?
+  #       res << TicTacToeNode.new(board, next_mark, [i, j])
+  #     end
+  #   end
+  # end
   end
 end
