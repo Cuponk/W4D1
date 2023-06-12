d5154dd55bd9744d67a9ee4fa8b6618194bc84c1
Weyman Leung <leungweyman@gmail.com>
2023-06-12 14:24:34 -0700

    add losing_node

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
@@ -10,10 +10,41 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
-    
+    if board.over?
+		# check winner to see whether evaluator is not winner
+		# and board has been won
+		if board.winner == evaluator || board.winner.nil?
+			return false
+		else
+			return true
+		end	
+	end
+	if self.next_mover_mark == evaluator
+		# go thru children and see whether they are losing
+		# if all are losing, then we know current player lost
+		self.children.all? {|child| child.losing_node?(evaluator)}
+	else
+		# next_mover_mark != evaluator, means opponents turn
+		# if there is a next move that causes lost they will do it
+		# if any losing node, opponent will play it
+		self.children.any? {|child| child.losing_node?(evaluator)}
+	end
   end
 
   def winning_node?(evaluator)
+	# if board winner is evaluator, is winning node
+	if board.over?
+		if board.winner == evaluator || board.winner.nil?
+			return false
+		else
+			return true
+		end	
+	end
+	if self.next_mover_mark == evaluator
+		self.children.all? {|child| child.losing_node?(evaluator)}
+	else
+		self.children.any? {|child| child.losing_node?(evaluator)}
+	end
   end
 
   # This method generates an array of all moves that can be made after
